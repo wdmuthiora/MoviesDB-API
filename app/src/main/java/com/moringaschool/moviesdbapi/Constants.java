26d72ed84a7723b5a64c951f500349e2da8b3fed
wdmuthiora <wdmuthiora@gmail.com>
2021-12-04 19:09:00 +0300

    Hid the api_key

---
+++ app/src/main/java/com/moringaschool/moviesdbapi/Constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.moviesdbapi;
+
+public class Constants {
+    public static final String MOVIE_DB_BASE_URL="https://api.themoviedb.org/3/";
+    public static final String MOVIE_DB_API=  BuildConfig.MOVIE_DB_API;
+}
---
+++ app/src/main/java/com/moringaschool/moviesdbapi/MainActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.moviesdbapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.moviesdbapi.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
