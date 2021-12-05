d9fcabd67efedac04ae38b55706de6690f18bca3
wdmuthiora <wdmuthiora@gmail.com>
2021-12-05 13:35:33 +0300

    Created PopularMovies adapter.

--- app/src/main/java/com/moringaschool/moviesdbapi/Adapters/PopularMoviesAdapter.java
+++ app/src/main/java/com/moringaschool/moviesdbapi/Adapters/PopularMoviesAdapter.java
@@ -1,4 +1,17 @@
 package com.moringaschool.moviesdbapi.Adapters;
 
+import android.content.Context;
+
+import com.moringaschool.moviesdbapi.Models.Movie;
+
+import java.util.List;
+
 public class PopularMoviesAdapter {
+    private Context mContext;
+    private List<Movie> mPopular;
+
+    public PopularMoviesAdapter(Context mContext, List<Movie> mPopular) {
+        this.mContext = mContext;
+        this.mPopular = mPopular;
+    }
 }
