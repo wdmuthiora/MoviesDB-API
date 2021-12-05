56b0919a56833b96a9c7ecdf376f5b8ff46f943e
wdmuthiora <wdmuthiora@gmail.com>
2021-12-05 15:43:47 +0300

    Bound the elements inside the MainActivity.

--- app/src/main/java/com/moringaschool/moviesdbapi/UI/MainActivity.java
+++ app/src/main/java/com/moringaschool/moviesdbapi/UI/MainActivity.java
@@ -3,14 +3,22 @@ package com.moringaschool.moviesdbapi.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.moringaschool.moviesdbapi.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class  MainActivity extends AppCompatActivity {
+    @BindView(R.id.progressBar) ProgressBar progressBar;
+    @BindView(R.id.moviesRecyclerView) ProgressBar moviesRecyclerView;
+    @BindView(R.id.tvErrorMessage) ProgressBar tvErrorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
