15664bba660fbffc4e2d0e9b4ee5a7b0cf47194e
wdmuthiora <wdmuthiora@gmail.com>
2021-12-05 13:52:28 +0300

    Bound elements to PopularMoviesViewHolder adapter.

--- app/src/main/java/com/moringaschool/moviesdbapi/Adapters/PopularMoviesAdapter.java
+++ app/src/main/java/com/moringaschool/moviesdbapi/Adapters/PopularMoviesAdapter.java
@@ -1,17 +1,39 @@
 package com.moringaschool.moviesdbapi.Adapters;
 
 import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.moringaschool.moviesdbapi.Models.Movie;
+import com.moringaschool.moviesdbapi.R;
 
 import java.util.List;
 
-public class PopularMoviesAdapter {
+import butterknife.BindView;
+
+public class PopularMoviesAdapter extends RecyclerView.ViewHolder {
     private Context mContext;
     private List<Movie> mPopular;
 
     public PopularMoviesAdapter(Context mContext, List<Movie> mPopular) {
         this.mContext = mContext;
         this.mPopular = mPopular;
+    }
+
+    public class PopularMoviesViewHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.ivMovieImage) ImageView ivMovieImage;
+
+        @BindView(R.id.tvReleaseDate) TextView mReleaseDate;
+
+        @BindView(R.id.tvMovieTitle) TextView tvMovieTitle;
+
+        @BindView(R.id.tvMovieDescription) TextView tvMovieDescription;
+
+        @BindView(R.id.tvMoviePopularity) TextView tvMoviePopularity;
+
+
     }
 }
