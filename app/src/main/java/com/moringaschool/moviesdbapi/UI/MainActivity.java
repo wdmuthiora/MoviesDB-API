76b7340f0ef4d50d23314a6d92fcd90dbc80c219
wdmuthiora <wdmuthiora@gmail.com>
2021-12-05 15:45:27 +0300

    Initialized the PopularMoviesAdapter and array of Movies inside the main Activity.

--- app/src/main/java/com/moringaschool/moviesdbapi/UI/MainActivity.java
+++ app/src/main/java/com/moringaschool/moviesdbapi/UI/MainActivity.java
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 
+import com.moringaschool.moviesdbapi.Adapters.PopularMoviesAdapter;
+import com.moringaschool.moviesdbapi.Models.Movie;
 import com.moringaschool.moviesdbapi.R;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class  MainActivity extends AppCompatActivity {
+
     @BindView(R.id.progressBar) ProgressBar progressBar;
     @BindView(R.id.moviesRecyclerView) ProgressBar moviesRecyclerView;
     @BindView(R.id.tvErrorMessage) ProgressBar tvErrorMessage;
+
+    private PopularMoviesAdapter mAdapter;
+    private List<Movie> mPopular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
