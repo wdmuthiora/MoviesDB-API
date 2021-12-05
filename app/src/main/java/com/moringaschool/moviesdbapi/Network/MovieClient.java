173142b448947d9c231238e2dce25ef2d2056880
wdmuthiora <wdmuthiora@gmail.com>
2021-12-05 09:59:52 +0300

    Create MovieClient class' getClient().

--- app/src/main/java/com/moringaschool/moviesdbapi/Network/MovieClient.java
+++ app/src/main/java/com/moringaschool/moviesdbapi/Network/MovieClient.java
@@ -1,7 +1,20 @@
 package com.moringaschool.moviesdbapi.Network;
 
+import android.graphics.Movie;
+
+import com.moringaschool.moviesdbapi.Constants;
+
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MovieClient {
+
     private static Retrofit retrofit=null;
+//Using client to build Retrofit instance, that will in turn be used to query the api
+    public static MovieApi getClient(){
+        if(retrofit==null){
+            retrofit=new Retrofit.Builder().baseUrl(Constants.MOVIE_DB_BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit.create(MovieApi.class );
+    }
 }
